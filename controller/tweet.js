a36e669f1589f82a28f18fb5115386da810ffabc
JangDragon <mnbv7952@naver.com>
2024-04-29 11:21:30 +0900

    Twitter 서버 리팩토링

---
+++ controller/tweet.js
@@ -0,0 +1,46 @@
+import * as tweetRepository from '../data/tweet.js'
+
+// 여러 트윗을 가져오는 함수
+export async function getTweets(req, res) {
+    const username = req.query.username
+    const data = await (username ? tweetRepository.getAllByUsername(username) : tweetRepository.getAll())
+    // username 유무에 따른 출력
+    res.status(200).json(data)
+}
+
+// 하나의 트윗을 가져오는 함수
+export async function getTweet(req, res, next) {
+    const id = req.params.id
+    const tweet = await tweetRepository.getById(id);
+    if(tweet) {
+        res.status(200).json(tweet)
+    }else{
+        res.status(404).json({message: `${id}의 트윕이 없습니다.`})
+    }
+}
+
+// 트윗 생성 함수
+export async function createTweet(req, res, next) {
+    const { text, name, username } = req.body;
+    const tweets = await tweetRepository.create(text, name, username);
+    res.status(201).json(tweets)
+}
+
+// 트윗 변경 함수
+export async function updateTweet(req, res, next) {
+    const id = req.params.id
+    const text = req.body.text
+    const tweet = await tweetRepository.update(id, text);
+    if(tweet){
+        res.status(201).json(tweet)
+    }else {
+        res.status(404).json({message: `${id}의 트윗이 없습니다.`})
+    }
+}
+
+// 트윗 삭제 함수
+export async function deleteTweet(req, res, next) {
+    const id = req.params.id
+    await tweetRepository.remove(id);
+    res.sendStatus(204)
+}
--- router/tweets.js
+++ router/tweets.js
@@ -1,121 +1,38 @@
 import express from 'express'
+import * as tweetController from '../controller/tweet.js'
 
 const router = express.Router()
-
-let tweets = [
-    {
-        id:'1',
-        text:'안녕하세요',
-        createdAt: Date.now().toString(),
-        name: '김사과',
-        username: 'apple',
-        url: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fwww.logoyogo.com%2Fweb%2Fwp-content%2Fuploads%2Fedd%2F2021%2F02%2Flogoyogo-1-45.jpg'
-    },
-    {
-        id:'2',
-        text:'반갑습니다',
-        createdAt: Date.now().toString(),
-        name: '반하나',
-        username: 'banana',
-        url: 'https://img.freepik.com/premium-vector/banana-cute-kawaii-style-fruit-character-vector-illustration_787461-1772.jpg'
-    }
-]
 
 // 해당 아이디에 트윗 가져오기
 // GET
 // http://localhost:8080/tweets?username=:username
-router.get('/', (req, res, next) => {
-    const username = req.query.username
-    const data = username ? tweets.filter((tweet) => tweet.username == username): tweets;
-    res.status(200).json(data)
-})
-/*
-router.get('/', (req, res) => {
-    const data = req.query.username
-    res.status(200).send(U_N(data))
-})
-function U_N(data) {
-    for (let i=0; tweets.length; i++){
-        if(tweets[i]["username"] == data){
-            return tweets[i]
-        }
-    }
-}
-*/
+router.get('/', tweetController.getTweets);
 
 
 // 글번호에 대한 트윗 가져오기
 // Get
 // http://localhost:8080/tweets/:id
-router.get('/:id', (req, res, next) => {
-    const id = req.params.id
-    const tweet = tweets.find((tweet) => tweet.id === id)
-    if(tweet) {
-        res.status(200).json(tweet)
-    }else{
-        res.status(404).json({message: `${id}의 트윕이 없습니다.`})
-    }
-})
-/*
-router.get('/:id', (req, res) => {
-    const data = req.params.id
-    res.status(200).send(ID(data))
-})
-function ID(data) {
-    for (let i=0; tweets.length; i++){
-        if(tweets[i]["id"] == data){
-            return tweets[i]
-        }
-    }
-}
-*/
+router.get('/:id', tweetController.getTweet);
+
 
 // 트윗하기(추가)
 // POST
 // http://localhost:8080/tweets
 // name, username, text
 // json 형태로 입력 후 추가된 데이터까지 모두 json으로 출력
-router.post('/', (req, res, next) => {
-    const { text, name, username } = req.body;
-    const tweet = {
-        id: '10',
-        text: text,
-        createdAt: Date.now().toString(),
-        name: name,
-        username: username,
-        url: 'https://img.freepik.com/premium-vector/banana-cute-kawaii-style-fruit-character-vector-illustration_787461-1772.jpg'
-    }
-    tweets= [tweet, ...tweets]
-    res.status(201).json(tweets)
-})
+router.post('/', tweetController.createTweet);
 
 // 트윗 수정하기
 // PUT
 // http://localhost:8080/tweets/:id
 // id, username, text
 // json 형태로 입력 후 변경된 데이터까지 모두 json으로 출력
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id
-    const username = req.body.username
-    const text = req.body.text
-    const tweet = tweets.find((tweet)=> tweet.id === id)
-    if(tweet){
-        tweet.username = username;
-        tweet.text = text;
-        res.status(201).json(tweet)
-    }else {
-        res.status(404).json({message: `${id}의 트윗이 없습니다.`})
-    }
-})
+router.put('/:id', tweetController.updateTweet);
 
 // 트윗 삭제하기
 // DELETE
 // http://localhost:8080/tweets/:id
-router.delete('/:id', (req, res, next) => {
-    const id = req.params.id
-    tweets = tweets.filter((tweet)=> tweet.id !== id)
-    res.sendStatus(204)
-    // res.status(204).json(tweets)
-})
+router.delete('/:id', tweetController.deleteTweet);
 
+    
 export default router
