00ebd4bd0c9c8b850a0d52426b38c885613b2d2b
JangDragon <mnbv7952@naver.com>
2024-05-17 11:45:20 +0900

    소캣 적용

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import tweetsRouter from './router/tweets.js'
 import authRouter from './router/auth.js'
 import { config } from './config.js'
 import { connectDB } from './db/database.js'
+import { initSocket } from "./connection/socket.js"
 
 const app = express()
 
@@ -13,13 +14,13 @@ app.use(morgan('dev'))
 app.use('/tweets', tweetsRouter)
 app.use('/auth', authRouter)
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.sendStatus(404)
 })
 
 // DB 연결 테스트!
 connectDB().then((db) => {
-    console.log('몽구스 사용하여 몽고디비 접속 성공')
-    app.listen(config.host.port)
+    const server = app.listen(config.host.port);
+    initSocket(server);
 }).catch(console.error);
 
--- controller/tweet.js
+++ controller/tweet.js
@@ -1,4 +1,6 @@
 import * as tweetRepository from '../data/tweet.js';
+import { getSocketIo } from '../connection/socket.js';
+
 // 여러 트윗을 가져오는 함수
 export async function getTweets(req, res){
     const username = req.query.username;
@@ -19,9 +21,9 @@ export async function getTweet(req, res, next) {
 // 트윗을 생성하는 함수
 export async function createTweet(req, res, next) {
     const { text } = req.body;
-    console.log(req.body)
     const tweet = await tweetRepository.create(text, req.body.userid);
     res.status(201).json(tweet);
+    getSocketIo().emit('tweets', tweet)
 }
 // 트윗을 변경하는 함수
 export async function updateTweet(req, res, next) {
